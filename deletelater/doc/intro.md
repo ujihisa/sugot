0f25525df5081d44aee4c9eae4ee63d430b786d4
ujihisa <ujihisa@gmail.com>
2015-07-18 22:30:06 -0700

    lein new app deletelater

---
+++ deletelater/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to deletelater
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ deletelater/test/deletelater/core_test.clj
@@ -0,0 +1,7 @@
+(ns deletelater.core-test
+  (:require [clojure.test :refer :all]
+            [deletelater.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
