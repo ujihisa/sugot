c007e2fb6176ed3c67b8b982af50300b118f656d
ujihisa <ujihisa@gmail.com>
2015-07-19 18:52:10 -0700

    register all

--- src/sugot/core.clj
+++ src/sugot/core.clj
@@ -17,6 +17,43 @@
                  :when klass]
              [klass [f]])))
 
+(defn defevent [pm event-type f]
+  (let [plugin (reify org.bukkit.plugin.Plugin
+                 (getConfig [this] nil)
+                 (getDatabase [this] nil)
+                 (getDataFolder [this] nil)
+                 (getDefaultWorldGenerator [this worldName id] nil)
+                 (getDescription [this] nil)
+                 (getLogger [this] nil)
+                 (getName [this] "sugot")
+                 (getPluginLoader [this] nil)
+                 (getResource [this filename] nil)
+                 (getServer [this] (Bukkit/getServer))
+                 (isEnabled [this] true)
+                 (isNaggable [this] nil)
+                 (onDisable [this] nil)
+                 (onEnable [this] (prn 'onEnable this))
+                 (onLoad [this] (prn 'onLoad this))
+                 (reloadConfig [this] nil)
+                 (saveConfig [this] nil)
+                 (saveDefaultConfig [this] nil)
+                 (saveResource [this resourcePath, replace-b] nil)
+                 (setNaggable [boolean canNag] nil))
+        listener (reify org.bukkit.event.Listener)
+        executor (reify org.bukkit.plugin.EventExecutor
+                   (execute [this listener event]
+                     (f event)))
+        priority org.bukkit.event.EventPriority/NORMAL]
+    (-> pm (.registerEvent event-type listener priority executor plugin))))
+
+(defn register-all [pm]
+  ; gather events, and register them
+  (doseq [app (apps)
+          :let [_ (require app)]
+          [klass fs] (listeners app)
+          f fs]
+    (defevent pm klass f)))
+
 (defn -main [& args]
   (future (Main/main (make-array String 0)))
 
@@ -25,6 +62,5 @@
     (Thread/sleep 100)
     (if server
       (let [pm (-> server .getPluginManager)]
-        ; gather events, and register them
-        )
+        (register-all pm))
       (recur (try (Bukkit/getServer) (catch Exception e nil))))) )
