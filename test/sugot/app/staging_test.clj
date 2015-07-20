4ea8d40b2ca4a5dc8857f6d64c001ac7cb9aa696
ujihisa <ujihisa@gmail.com>
2015-07-19 21:12:46 -0700

    (dummy fixture for future)

--- test/sugot/app/staging_test.clj
+++ test/sugot/app/staging_test.clj
@@ -11,3 +11,18 @@
           ^java.net.InetAddress real-address nil]
       (PlayerLoginEvent (org.bukkit.event.player.PlayerLoginEvent. player "dummy-hostname" address result "dummy-message" real-address)))
     #_ "TODO add assertions"))
+
+(deftest PlayerQuitEvent-test
+  (testing "PlayerQuitEvent is nice"
+    (let [player (reify org.bukkit.entity.Player
+                   (getName [this] "dummy-player"))]
+      (PlayerQuitEvent (org.bukkit.event.player.PlayerQuitEvent. player "dummy-message")))
+    #_ "TODO add assertions"))
+
+(defn fixture [f]
+  ; before
+  (f)
+  ; after
+  )
+
+(use-fixtures :each f)
