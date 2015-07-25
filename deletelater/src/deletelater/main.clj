1935df7ecad085dc3fe08dbb89c54039104a14b2
ujihisa <ujihisa@gmail.com>
2015-07-24 17:17:05 -0700

    git rm -r deletelater

--- deletelater/src/deletelater/main.clj
+++
@@ -1,7 +0,0 @@
-(ns deletelater.main
-  (:import [org.bukkit.event.player AsyncPlayerChatEvent])
-  (:gen-class
-    :extends org.bukkit.plugin.java.JavaPlugin))
-
-(defn f []
-  (prn 'hi AsyncPlayerChatEvent))
