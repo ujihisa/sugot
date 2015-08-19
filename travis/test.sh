ccbeedc8925fcb252413688dd7dedcb5b13a4708
ujihisa <tatsuhiro.ujihisa@hootsuite.com>
2015-08-19 00:01:02 -0700

    better travising

--- travis/test.sh
+++ travis/test.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash -ex
 mkdir -p spigot
 cd spigot
 pwd
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-java -Xms512m -Xmx1g -jar BuildTools.jar
+java -Xms512m -Xmx1g -jar BuildTools.jar > /dev/null
 cd ..
 ln -s spigot/spigot-1.8.8.jar .
-expect travis/deploy.expect
+lein deploy localrepo1 org.spigotmc/spigot 1.8.8 spigot-1.8.8.jar
 lein test
