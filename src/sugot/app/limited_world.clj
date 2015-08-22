6583782e4614e48bc620395d83d9c35596c1e0ff
ujihisa <ujihisa@gmail.com>
2015-08-21 23:20:59 -0700

    yay

--- src/sugot/app/limited_world.clj
+++ src/sugot/app/limited_world.clj
@@ -5,13 +5,11 @@
   NOTE: This doesn't run very often just for performance"
   [event p]
   (when (= 0 (rand-int 10))
-    (try
-      (let [x (-> p :loc :x)
-            z (-> p :loc :z)]
-        (cond
-          (or (< x -300) (< 300 x))
-          (.sendMessage (:orig p) (format "Your current x is %d. Go back within -300 <= x <= 300") (int x))
+    (let [x (-> p :loc :x)
+          z (-> p :loc :z)]
+      (cond
+        (or (< x -300) (< 300 x))
+        (.sendMessage (:orig p) (format "Your current x is %d. Go back within -300 <= x <= 300" (int x)))
 
-          (or (< z -300) (< 300 z))
-          (.sendMessage (:orig p) (format "Your current z is %d. Go back within -300 <= z <= 300") (int z))))
-      (catch Exception e (.printStackTrace e)))))
+        (or (< z -300) (< 300 z))
+        (.sendMessage (:orig p) (format "Your current z is %d. Go back within -300 <= z <= 300" (int z)))))))
