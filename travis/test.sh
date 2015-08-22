b26ae11964b60e971c38284188befe9897eb50bc
ujihisa <ujihisa@gmail.com>
2015-08-21 23:13:11 -0700

    version var

--- travis/test.sh
+++ travis/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
+VERSION="1.8.8"
 mkdir -p spigot
 cd spigot
 pwd
+rm -f BuildTools.jar
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -Xms512m -Xmx1g -jar BuildTools.jar > /dev/null
 cd ..
-ln -s spigot/spigot-1.8.8.jar .
-lein deploy localrepo1 org.spigotmc/spigot 1.8.8 spigot-1.8.8.jar
+ln -s "spigot/spigot-${VERSION}.jar" .
+lein deploy localrepo1 org.spigotmc/spigot "${VERSION}" "spigot-${VERSION}.jar"
 lein test
