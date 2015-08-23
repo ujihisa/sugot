b0ebd149318b73c398fdeac4b17f9cc5c42f9964
ujihisa <ujihisa@gmail.com>
2015-08-22 21:35:26 -0700

    one person can lead to morning

--- src/sugot/app/staging.clj
+++ src/sugot/app/staging.clj
@@ -1,3 +1,10 @@
 (ns sugot.app.staging
   (:require [clojure.string :as s]
             [sugot.lib :as l]))
+
+(defn PlayerBedEnterEvent [event p]
+  (l/later (l/sec 2)
+           (let [world (-> p :orig .getLocation .getWorld )]
+             (when (< 12541 (.getTime world) 23458)
+               (l/broadcast "[BED] Good morning!")
+               (.setTime world 0)))))
