5e1c511584e7a71f781ee33e66901e4cd02edc99
ujihisa <ujihisa@gmail.com>
2015-08-24 22:23:33 -0700

    import org.

--- test/sugot/app/convo_test.clj
+++ test/sugot/app/convo_test.clj
@@ -2,12 +2,7 @@
   (:require [clojure.test :refer :all]
             [sugot.app.convo :refer :all]
             [sugot.lib :as l]
-            [sugot.models]
-            [sugot.mocks :as mocks])
-  (:import [org.bukkit.craftbukkit Main]
-           [org.bukkit Bukkit]
-           [org.bukkit.entity Player]
-           [sugot.models Loc P]))
+            [sugot.mocks :as mocks]))
 
 (deftest replacefirst-go-test
   (testing "a word"
@@ -36,20 +31,3 @@
                  (getFormat [this] "<%s> %s")
                  (setMessage [this msg]))
                nil))))))
-
-#_ (defn fixture [f]
-  (future (Main/main (make-array String 0)))
-
-  ; call `f` once server is ready.
-  (loop [server nil]
-    (Thread/sleep 100)
-    (if server
-      (f)
-      (recur (try (Bukkit/getServer) (catch Exception e nil)))))
-
-  ; dirty hack: wait until report completes.
-  (future
-    (Thread/sleep 100)
-    (System/exit 0)))
-
-#_ (use-fixtures :once fixture)
