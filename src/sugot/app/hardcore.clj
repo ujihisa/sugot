871f1612c85078c33a56390961d09c9a878125cf
ujihisa <ujihisa@gmail.com>
2015-09-05 18:00:18 -0700

    GC

--- src/sugot/app/hardcore.clj
+++ src/sugot/app/hardcore.clj
@@ -7,7 +7,7 @@
            [org.bukkit.event.block Action]
            [org.bukkit.event.entity CreatureSpawnEvent$SpawnReason]))
 
-(defn exist? []
+(defn- hardcore-world-exist? []
   (.isDirectory (clojure.java.io/as-file "hardcore")))
 
 (defn in-hardcore? [loc]
@@ -18,7 +18,7 @@
         reason (.getSpawnReason event)
         l (.getLocation event)]
     ; You can't use `case` for Java enum
-    (when (and (exist?)
+    (when (and (hardcore-world-exist?)
                (in-hardcore? l)
                (instance? org.bukkit.entity.Monster entity)
                (= CreatureSpawnEvent$SpawnReason/NATURAL reason))
@@ -34,7 +34,7 @@
    ])
 
 (defn create []
-  {:pre [(not (exist?))]}
+  {:pre [(not (hardcore-world-exist?))]}
   (let [world-creator (-> (WorldCreator. "hardcore")
                         (.copy (Bukkit/getWorld "world"))
                         (.seed (rand-int 10000)))
@@ -77,21 +77,15 @@
               (contains? #{Action/RIGHT_CLICK_AIR Action/RIGHT_CLICK_BLOCK} action)
               (enter-satisfy? player))
         (l/send-message player "[HARDCORE] OK")
-        (when-not (exist?)
+        (when-not (hardcore-world-exist?)
           (create))
         (enter player)))))
 
-#_ (defn TODO [player-name]
-  (when-let [player (Bukkit/getPlayer player-name)]
-    (let [hardcore-world (world)
-          spawn-loc (.getSpawnLocation hardcore-world)]
-      (.teleport player spawn-loc)
-      (.teleport (Bukkit/getPlayer "mozukusoba") spawn-loc)
-      (.teleport (Bukkit/getPlayer "kamichidu") spawn-loc))
-    #_ (.teleport player (.getBedSpawnLocation player))
-    #_ (Bukkit/unloadWorld "hardcore" false)))
-
-
-#_ (l/later 0 (TODO "ujm"))
-#_ (l/later 0 (TODO "mozukusoba"))
-#_ (l/later 0 (TODO "kamichidu"))
+(defn garbage-collection []
+  (when (and
+          (hardcore-world-exist?)
+          (not (some in-hardcore?
+                     (map #(.getLocation %) (Bukkit/getOnlinePlayers)))))
+    (Bukkit/unloadWorld "hardcore" false)
+    ; TODO remove the dir
+    ))
