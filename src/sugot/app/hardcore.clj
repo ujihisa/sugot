14ee1954323e774ae5c03d952d9a4a5e409c330e
ujihisa <ujihisa@gmail.com>
2015-09-05 23:33:42 -0700

    hahaha

--- src/sugot/app/hardcore.clj
+++ src/sugot/app/hardcore.clj
@@ -2,7 +2,7 @@
   (:require [sugot.lib :as l]
             [sugot.block :as b]
             [sugot.world])
-  (:import [org.bukkit Bukkit Server WorldCreator Material]
+  (:import [org.bukkit Bukkit Server WorldCreator Material Location]
            [org.bukkit.entity ArmorStand]
            [org.bukkit.event.block Action]
            [org.bukkit.event.entity CreatureSpawnEvent$SpawnReason]
@@ -42,6 +42,12 @@
    5574457897082764526 ; http://epicminecraftseeds.com/sweet-savanna-m-above-the-clouds-minecraft-1-8-seed/
    ])
 
+(defn random-xz [radius]
+  (let [x (- (rand-int (* 2 radius)) radius)
+        z (Math/round (* (Math/sqrt (- (* radius radius) (* x x)))
+                         (rand-nth [1 -1])))]
+    [x z]))
+
 (defn create []
   {:pre [(not (hardcore-world-exist?))]}
   (let [world-creator (-> (WorldCreator. "hardcore")
@@ -50,27 +56,32 @@
                                #_ (first interesting-seeds)))
         hardcore-world (.createWorld world-creator)]
     (.setTime hardcore-world 21000)
-    (let [spawn-loc (.getSpawnLocation hardcore-world)
-          x (.getX spawn-loc)
-          z (.getZ spawn-loc)
-          highest-y (.getHighestBlockYAt hardcore-world x z)]
-      (.setSpawnLocation hardcore-world x (inc highest-y) z)
+    (let [[goal-x goal-z] (random-xz 50)
+          goal-y (.getHighestBlockYAt hardcore-world goal-x goal-z)
+          init-y (.getHighestBlockYAt hardcore-world 0 0)]
+      (.setSpawnLocation hardcore-world goal-x (inc goal-y) goal-z)
       (l/later 0
-               (b/set-block (.getBlockAt hardcore-world x highest-y z)
+               (b/set-block (.getBlockAt hardcore-world goal-x goal-y goal-z)
+                            Material/BEDROCK 0)
+               (b/set-block (.getBlockAt hardcore-world 0 init-y 0)
                             Material/OBSIDIAN 0)
-               (b/set-block (.getBlockAt hardcore-world x (inc highest-y) z)
+               (b/set-block (.getBlockAt hardcore-world 0 init-y 0)
                             Material/TORCH 0)))))
 
 (defn hardcore-world []
   (Bukkit/getWorld "hardcore"))
 
 (defn enter-hardcore [player]
-  (let [spawn-loc (.getSpawnLocation (hardcore-world))]
-    (.teleport player spawn-loc)
+  {:pre [(hardcore-world)]}
+  (let [init-loc
+        (Location.
+          (hardcore-world) 0 (.getHighestBlockYAt (hardcore-world) 0 0) 0)]
+    (.teleport player init-loc)
     (l/broadcast-and-post-lingr
-      (format "[HARDCORE] %s entered to hardcore world. (seed: %d)"
+      (format "[HARDCORE] %s entered to hardcore world. (seed: \"%d\", biome: %s)"
               (.getName player)
-              (.getSeed (hardcore-world))))))
+              (.getSeed (hardcore-world))
+              (.name (.getBiome (hardcore-world) 0 0))))))
 
 (defn enter-satisfy? [player]
   (when-let [item-stack (.getItemInHand player)]
@@ -84,7 +95,7 @@
 (defn leave-satisfy? [player]
   (when-let [item-stack (.getItemInHand player)]
     (when (and (in-hardcore? (.getLocation player))
-               (= Material/MAP (.getType item-stack))
+               (= Material/COMPASS (.getType item-stack))
                (= 1 (.getAmount item-stack)))
       (when-let [armour-stand (some #(when (instance? ArmorStand %) %)
                                     (.getNearbyEntities player 0 0 0))]
@@ -105,7 +116,7 @@
         (cond
           (enter-satisfy? player)
           (do
-            (.setItemInHand player (ItemStack. Material/MAP 1))
+            (.setItemInHand player (ItemStack. Material/COMPASS 1))
             (when-not (hardcore-world-exist?)
               (l/broadcast "[HARDCORE] (Creating world...)")
               (create))
