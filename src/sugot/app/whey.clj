5058ff60566b5ddf5c7aa5005e3920e309cf9f49
ujihisa <ujihisa@gmail.com>
2015-09-13 23:20:44 -0700

    wip

--- src/sugot/app/whey.clj
+++ src/sugot/app/whey.clj
@@ -11,10 +11,14 @@
                      (l/set-display-name "Whey"))
         milk->whey (doto (ShapelessRecipe. item-stack)
                      (.addIngredient Material/MILK_BUCKET))
-        item-stack (doto (ItemStack. Material/MILK_BUCKET 1)
+        item-stack (doto (ItemStack. Material/SUGAR 1)
                      (.addUnsafeEnchantment Enchantment/DURABILITY 1)
                      (l/set-display-name "Ricotta"))
         whey->ricotta (FurnaceRecipe. item-stack
                                       Material/MILK_BUCKET
                                       (byte 1))]
     [milk->whey whey->ricotta]))
+
+(defn PlayerBucketEmptyEvent [event]
+  (let [player (.getPlayer event)]
+    :ok))
