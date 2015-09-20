444ff908fa0d0899cd0c21bf7c24977dde8b07ea
ujihisa <ujihisa@gmail.com>
2015-09-20 15:08:54 -0700

    simplify

--- src/sugot/app/elevator.clj
+++ src/sugot/app/elevator.clj
@@ -74,12 +74,8 @@
         ydiff (elevator-mover-f elevator)]
     (l/later 0 (move-entities entities ydiff))))
 
-(defn up-elevator
-  "Raise the given elevator as an side effect,
-  and returns new location's y-diff where player should teleport."
-  [elevator]
-  (let [ydiff 1]
-    (b/set-block! (b/from-loc (:loc-plate elevator) 0 0 0)
+(defn move-elevator [elevator ydiff]
+  (b/set-block! (b/from-loc (:loc-plate elevator) 0 0 0)
                   Material/AIR
                   1)
     (doseq [x (range -1 2)
@@ -95,32 +91,20 @@
                   1)
     (b/set-block! (b/from-loc (:loc-bar elevator) 0 -1 0)
                   Material/IRON_FENCE
-                  0)
-    ; TODO
+                  0))
+(defn up-elevator
+  "Raise the given elevator as an side effect,
+  and returns new location's y-diff where player should teleport."
+  [elevator]
+  (let [ydiff 1]
+    (move-elevator elevator ydiff)
     ydiff))
 
 (defn down-elevator
   "similar to `up-elevator`"
   [elevator]
   (let [ydiff -1]
-    (b/set-block! (b/from-loc (:loc-plate elevator) 0 0 0)
-                  Material/AIR
-                  1)
-    (doseq [x (range -1 2)
-            z (range -1 2)]
-      (b/set-block! (b/from-loc (:loc-plate elevator) x (dec ydiff) z)
-                    (:base-type elevator)
-                    (:base-data elevator))
-      (b/set-block! (b/from-loc (:loc-plate elevator) x -1 z)
-                    Material/AIR
-                    0))
-    (b/set-block! (b/from-loc (:loc-plate elevator) 0 ydiff 0)
-                  Material/STONE_PLATE
-                  1)
-    (b/set-block! (b/from-loc (:loc-bar elevator) 0 -1 0)
-                  Material/IRON_FENCE
-                  0)
-    ; TODO
+    (move-elevator elevator ydiff)
     ydiff))
 
 (defn PlayerMoveEvent [event]
