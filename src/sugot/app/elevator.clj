09151198d4cdab7f55615deb1dfd36c606c16639
ujihisa <ujihisa@gmail.com>
2015-09-20 15:06:03 -0700

    and go down

--- src/sugot/app/elevator.clj
+++ src/sugot/app/elevator.clj
@@ -79,18 +79,45 @@
   and returns new location's y-diff where player should teleport."
   [elevator]
   (let [ydiff 1]
+    (b/set-block! (b/from-loc (:loc-plate elevator) 0 0 0)
+                  Material/AIR
+                  1)
     (doseq [x (range -1 2)
             z (range -1 2)]
       (b/set-block! (b/from-loc (:loc-plate elevator) x (dec ydiff) z)
                     (:base-type elevator)
                     (:base-data elevator))
-      (b/set-block! (b/from-loc (:loc-plate elevator) x (- ydiff 2) z)
+      (b/set-block! (b/from-loc (:loc-plate elevator) x -1 z)
                     Material/AIR
                     0))
     (b/set-block! (b/from-loc (:loc-plate elevator) 0 ydiff 0)
                   Material/STONE_PLATE
                   1)
-    (b/set-block! (b/from-loc (:loc-bar elevator) 0 (- ydiff 2) 0)
+    (b/set-block! (b/from-loc (:loc-bar elevator) 0 -1 0)
+                  Material/IRON_FENCE
+                  0)
+    ; TODO
+    ydiff))
+
+(defn down-elevator
+  "similar to `up-elevator`"
+  [elevator]
+  (let [ydiff -1]
+    (b/set-block! (b/from-loc (:loc-plate elevator) 0 0 0)
+                  Material/AIR
+                  1)
+    (doseq [x (range -1 2)
+            z (range -1 2)]
+      (b/set-block! (b/from-loc (:loc-plate elevator) x (dec ydiff) z)
+                    (:base-type elevator)
+                    (:base-data elevator))
+      (b/set-block! (b/from-loc (:loc-plate elevator) x -1 z)
+                    Material/AIR
+                    0))
+    (b/set-block! (b/from-loc (:loc-plate elevator) 0 ydiff 0)
+                  Material/STONE_PLATE
+                  1)
+    (b/set-block! (b/from-loc (:loc-bar elevator) 0 -1 0)
                   Material/IRON_FENCE
                   0)
     ; TODO
@@ -112,13 +139,10 @@
                                  (.add 0 (+ 0.01 y-diff) 0)))))))))
 
 (defn PlayerToggleSneakEvent [event]
-  #_ (let [player (.getPlayer event)
+  (let [player (.getPlayer event)
         loc (.getLocation player)]
     (when (.isSneaking event)
       (when-let [elevator (get-elevator-from loc)]
         (l/send-message player (format "[ELEVATOR] going down. %s"
                                        (prn-str elevator)))
-        (let [y-diff -1]
-          (when (move-elevator elevator y-diff)
-            (l/teleport player (doto (.getLocation player)
-                                 (.add 0 (+ 0.01 y-diff) 0)))))))))
+        (move-elevator-and-entities elevator down-elevator)))))
