ef4c9d2a04001ce82b31585f92ba11f2e032091e
ujihisa <ujihisa@gmail.com>
2016-08-16 23:28:18 -0700

    1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sugot "1.7"
+(defproject sugot "1.8"
   :description "clojure + spigot minecraft server"
   :url "https://github.com/ujihisa/sugot"
   :license {:name "GPL3 or any later versions"
