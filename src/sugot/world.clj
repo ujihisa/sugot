1fa7e1b70d5544eb6f7098606da4a85361326ff7
ujihisa <ujihisa@gmail.com>
2016-08-16 21:59:46 -0700

    Oh spigot..

--- src/sugot/world.clj
+++ src/sugot/world.clj
@@ -1,10 +1,14 @@
-(ns sugot.world)
+(ns sugot.world
+  (:import [org.bukkit Sound]))
 
 (defn strike-lightning-effect [loc]
   (.strikeLightningEffect (.getWorld loc) loc))
 
 (defn play-sound [loc sound volume pitch]
-  (.playSound (.getWorld loc) loc sound (float volume) (float pitch)))
+  ; Looks like World.playSound as of 2016-08-16 is buggy. Wrap it with try here.
+  (try
+    (.playSound (.getWorld loc) loc ^Sound sound (float volume) (float pitch))
+    (catch Exception e e)))
 
 (defn play-effect [loc effect data]
   (.playEffect (.getWorld loc) loc effect data))
