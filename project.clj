8851c48176ea473972acd34edbfaf0c8a9bac6e6
ujihisa <ujihisa@gmail.com>
2016-08-16 21:47:18 -0700

    Release as 1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sugot "1.4"
+(defproject sugot "1.5"
   :description "clojure + spigot minecraft server"
   :url "https://github.com/ujihisa/sugot"
   :license {:name "GPL3 or any later versions"
